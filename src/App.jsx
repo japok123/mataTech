91b8d3d8c472438f8e1c551964839a119a380ebf
Japs <94045399+Japs32@users.noreply.github.com>
2024-11-11 23:46:52 +0800

    new project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+  <div>
+    <h1 className="text-6xl font-bold underline">
+        Hello world!
+      </h1>
+  </div>
+  )
+}
+
+export default App
